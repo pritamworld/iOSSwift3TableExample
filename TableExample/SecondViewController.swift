045ba0de18e8e1155483ae8e77302f7d4cd50ed4
pritesh.patel <pritam.world@gmail.com>
2017-05-09 11:43:00 -0400

    Social Integration added

--- TableExample/SecondViewController.swift
+++ TableExample/SecondViewController.swift
@@ -7,13 +7,16 @@
 //
 
 import UIKit
+import Social
 
 protocol MyProtocol {
     func getBack(controller:SecondViewController, str: String) ;
 }
 
 class SecondViewController: UIViewController {
-
+    
+    
+    
     @IBOutlet weak var lblDummy: UILabel!
     var i:Int=0
     var screenWidth:CGFloat=0
@@ -23,7 +26,7 @@ class SecondViewController: UIViewController {
         super.viewDidLoad()
         print("Changed")
         i = 100;
-NotificationCenter.default.addObserver(self, selector: #selector(SecondViewController.rotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(SecondViewController.rotated), name: NSNotification.Name.UIDeviceOrientationDidChange, object: nil)
         // Do any additional setup after loading the view.
     }
     
@@ -35,7 +38,7 @@ NotificationCenter.default.addObserver(self, selector: #selector(SecondViewContr
         } else {
             print("Portrait %d",i)
             i = 300
-             lblDummy.isHidden = false
+            lblDummy.isHidden = false
         }
         getScreenSize()
     }
@@ -62,8 +65,8 @@ NotificationCenter.default.addObserver(self, selector: #selector(SecondViewContr
             print("Portrait")
         }
     }
-
-
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -73,15 +76,86 @@ NotificationCenter.default.addObserver(self, selector: #selector(SecondViewContr
         delegate?.getBack(controller: self,str: "Pritesh Patel Click to close this view")
         self.dismiss(animated: false, completion: nil)
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    @IBAction func shareFacebook(_ sender: AnyObject) {
+        
+        
+        if(SLComposeViewController.isAvailable(forServiceType: SLServiceTypeFacebook)){
+            let fbSheet = SLComposeViewController(forServiceType: SLServiceTypeFacebook)
+            fbSheet?.setInitialText("initial text")
+            //fbSheet?.add(image)
+            fbSheet?.add(URL.init(string: "http://yourlink.com"))
+            fbSheet?.completionHandler = {(result)  in
+                switch(result) {
+                //  This means the user cancelled without sending the Tweet
+                case SLComposeViewControllerResult.cancelled:
+                    break;
+                //  This means the user hit 'Send'
+                case SLComposeViewControllerResult.done:
+                    
+                    break;
+                }
+                
+                
+            }
+            if (fbSheet != nil){
+                present(fbSheet!, animated: true, completion: nil)
+            }
+            
+        }else{
+            let alert = UIAlertController(title: "Log in to Facebook", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
     }
-    */
-
+    
+    
+    @IBAction func shareTwitter(_ sender: AnyObject) {
+        
+        if(SLComposeViewController.isAvailable(forServiceType: SLServiceTypeTwitter)){
+            let tweetSheet = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            tweetSheet?.setInitialText("initial text")
+            //tweetSheet?.add(image)
+            tweetSheet?.add(URL.init(string: "http://yourlink.com"))
+            tweetSheet?.completionHandler = {(result)  in
+                switch(result) {
+                //  This means the user cancelled without sending the Tweet
+                case SLComposeViewControllerResult.cancelled:
+                    break;
+                //  This means the user hit 'Send'
+                case SLComposeViewControllerResult.done:
+                    
+                    break;
+                }
+                
+                
+            }
+            if (tweetSheet != nil){
+                present(tweetSheet!, animated: true, completion: nil)
+            }
+            
+        }else{
+            let alert = UIAlertController(title: "Log in to Twitter", message: nil, preferredStyle: UIAlertControllerStyle.alert)
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    
 }
